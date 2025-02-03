8e21c8aad320f74c1236bc48c6c966334b914219
Vladimir Zhidkov <v.zhidkov@innopolis.university>
2025-02-03 17:09:23 +0300

    Initial commit

---
+++ src/main/java/com/example/aviaScanner/AviaScannerApplication.java
@@ -0,0 +1,13 @@
+package com.example.aviaScanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AviaScannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AviaScannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/aviaScanner/AviaScannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aviaScanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AviaScannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
